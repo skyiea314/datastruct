db64d5ffead1b8a20075b64527cd2eb43196a910
skyiea <1929615109@qq.com>
2019-10-25 23:41:34 +0800

    添加顺序存储

---
+++ 线性表/顺序存储/SqList.c
@@ -0,0 +1,20 @@
+#define Maxsize 50
+typedef int Elemtype;
+typedef struct{
+	ElemTyoe data[Maxsize];
+	int length;
+	
+}SqlList;
+//¾²Ì¬
+typedef ElemType;
+typedef struct{
+	ElemType *date;
+	int Maxsize,length;
+
+
+}SqList;
+
+#define initsize 100
+SqList L;
+L.data = (ElemType*)mallco((ElemTyoe)*initsize);
+//¶¯Ì¬
